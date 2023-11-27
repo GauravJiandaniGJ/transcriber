a9f7172be5fabb96906e020c8e05894c56292bec
Israe17 <israelmahida.todoit@gmail.com>
2023-11-27 10:12:25 +0530

    Issue solved related to Urls

--- audio_transcribe/urls.py
+++ audio_transcribe/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from transcribe_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.transcribe_audio, name='transcribe_audio'),
+    path("admin/", admin.site.urls),
+    path("", views.transcribe_audio_view, name="transcribe_audio"),
 ]
